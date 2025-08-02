dc5d9538a33aaa4468fcdd0b64bcf23db6fe3434
vaibhavgaur <110227490+vaibhav4gaur@users.noreply.github.com>
2025-08-02 18:01:43 +0530

    Add files via upload

---
+++ data_hiding.py
@@ -0,0 +1,18 @@
+class simple:
+    def __init__(self):
+        self.value1 = 1
+        self.value2 = 2
+
+    def __A1_(self):
+        print("apple")
+
+    def _B1_(self):
+        print("banana")
+
+s = simple()
+print(s.value1)
+s._simple__A1_()
+
+print(dir(s))
+
+# __
