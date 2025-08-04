15ee88dcfbc681c9a6c2757c9869312f6e053ae0
vaibhavgaur <110227490+vaibhav4gaur@users.noreply.github.com>
2025-08-04 10:58:02 +0530

    Update errors.py

--- errors.py
+++ errors.py
@@ -1,31 +1,39 @@
-'''
-#1 : Syntax error
+# 1 : Syntax error
+
 a=2
 if a<1:
     print('Hello')
 else:
     print('Welcome')
+    
 # 2 : identationn error
+
 a=2
+
 if a<1:
+    
     print('Hello')
 else:
     print('Welcome')
 
 # 3 : zero division error
+
 print(7/0)
 
 # 4: module error
+
 import mathematics
 print(mathematics.pi)
 
-#5 : type error
+# 5 : type error 
+
 a=4
 b=8
 print(a+b)
 
-#6 : Logic error
+# 6 : Logic error 
+
 a=5
 c=3
 print(a*c)
-'''
+
